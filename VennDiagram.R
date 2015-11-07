a0d3a85bdaf276cdd6570e3a7a748bfb25e67e17
Ryan Stanley <rstanley@mun.ca>
2015-11-07 00:48:21 -0400

    Added code and data
    Code template for Venn plots
    Data of the outlier loci from each method

---
+++ VennDiagram.R
@@ -0,0 +1,21 @@
+## load libraries --------
+library(VennDiagram)
+
+## load data -------
+OutData <- read.csv("Outliers.csv",header=T,sep=",")
+
+#Parse data --------
+BayeScan <- as.vector(OutData[which(OutData$Method=="BayeScan"),"Loci"])
+Arelquin <- as.vector(OutData[which(OutData$Method=="Arl"),"Loci"])
+ArelquinIsland <- as.vector(OutData[which(OutData$Method=="ArlHeir"),"Loci"])
+
+# number of overlapping predictions among all methods
+length(Reduce(intersect,list(BayeScan,Arelquin,ArelquinIsland)))
+
+##Create venn diagram ---------
+venn.diagram(list(BayeScan,ArelquinIsland,Arelquin),
+             "Outlier_Venn.tiff",
+             fill=c("white","grey90","lightblue"),
+             category=c("BS","AH","ANH"),
+             rotation.degree=0.45)
+
