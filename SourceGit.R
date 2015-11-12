7f6c04146d8368169fede32dda67c2dc0881667a
Ryan Stanley <rstanley@mun.ca>
2015-11-12 12:12:22 -0400

    update

--- SourceGit.R
+++ SourceGit.R
@@ -1,8 +1,8 @@
 SourceGitFunc <- function(url)
 {
 ## URL is the raw format link from Github online 
-## e.g. "https://raw.githubusercontent.com/rystanley/Rad_R_Functions/master/GenePopConvert.R"
+## e.g. "https://raw.githubusercontent.com/rystanley/RAD_R_Functions/master/GenoPopConvert.R"
 require(RCurl)
-script <- getURL(url)
+script <- getURL(url, ssl.verifypeer = FALSE)
 eval(parse(text = script))
 }
