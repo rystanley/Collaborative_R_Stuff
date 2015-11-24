de1da0736b1c5afe7ebd3fdf87b213c3af2c328b
rystanley <rstanley@mun.ca>
2015-11-23 22:27:29 -0400

    changes

--- Structure_BGC_Conversion_GreenCrab.R
+++ Structure_BGC_Conversion_GreenCrab.R
@@ -13,14 +13,10 @@ setwd("~BGC/")
 #Load in geneteic data
 CrabNorth <- import2genind('CrabOutliers_PureNorth.str', onerowperind=F, n.ind=132, n.loc=117, row.marknames=1, col.lab=1, col.pop=2, ask=F)
 CrabSouth <- import2genind('CrabOutliers_PureSouth.str', onerowperind=F, n.ind=66, n.loc=117, row.marknames=1, col.lab=1, col.pop=2, ask=F)
-CrabMixed <- import2genind('Outliers_AdmixedIn.str', onerowperind=F, n.ind=43, n.loc=117, row.marknames=1, col.lab=1, col.pop=2, ask=F)
 
 #convert to adegenet format
 CrabNorth <- genind2genpop(CrabNorth)
 CrabSouth <- genind2genpop(CrabSouth)
-CrabMixed <- genind2genpop(CrabMixed) ### I think you have a monomophic allele or something. Not an even number of alleles. 
-
-head(CrabMixed) # should be ab 1 x 230 matrix 
 
 #Grab the allele frequency format data * the code on the web is missing something
 CN_Data=t(CrabNorth@tab)
@@ -33,13 +29,8 @@ CrabSouth_allele <- data.frame(Allele=rownames(CS_Data))
 CrabSouth_allele <- cbind(CrabSouth_allele,CS_Data)
 rownames(CrabSouth_allele) <- NULL
 
-CM_Data=t(CrabMixed@tab)
-CrabMixed_allele <- data.frame(Allele=rownames(CM_Data))
-CrabMixed_allele <- cbind(CrabMixed_allele,CM_Data)
-rownames(CrabMixed_allele) <- NULL ### ** issue here. There seems to be a missing allele for one of the loci
-
 #remove unneeded data from this step
-rm(CN_Data,CS_Data,CM_Data)
+rm(CN_Data,CS_Data)
 
 #Get locus names without the allele appended value
 LocusNorth <- as.character(CrabNorth_allele$Allele)
@@ -52,21 +43,12 @@ LocusSouth <- lapply(strsplit(LocusSouth, '.', fixed=TRUE), '[[', 1)
 LocusSouth <- LocusSouth[rep(c(TRUE,FALSE),nrow(CrabSouth_allele)/2)]
 LocusSouth <- unlist(LocusSouth)
 
-LocusMixed <- as.character(CrabMixed_allele$Allele)
-LocusMixed <- lapply(strsplit(LocusMixed, '.', fixed=TRUE), '[[', 1)
-table(unlist(LocusMixed))[which(as.vector(table(unlist(LocusMixed)))==1)] # here we see the issue with locus 3657 5488 6798 7068  946 all only have one allele
-LocusMixed <- LocusMixed[rep(c(TRUE,FALSE),nrow(CrabMixed_allele)/2)]
-LocusMixed <- unlist(LocusMixed)
-
 #Get the data for each allele in each population (every other row as denoted by even and odd row integers)
 North_Odd <- CrabNorth_allele[seq(1,length(CrabNorth_allele$Allele),2),]
 North_Even <- CrabNorth_allele[seq(2,length(CrabNorth_allele$Allele),2),]
 
 South_Odd <- CrabSouth_allele[seq(1,length(CrabSouth_allele$Allele),2),]
 South_Even <- CrabSouth_allele[seq(2,length(CrabSouth_allele$Allele),2),]
-
-Mixed_Odd <- CrabMixed_allele[seq(1,length(CrabMixed_allele$Allele),2),]
-Mixed_Even <- CrabMixed_allele[seq(2,length(CrabMixed_allele$Allele),2),]
 
 #Create dataframes ## here is where I am confused. Is there supposed to be a new text file for each population?
 
@@ -91,11 +73,87 @@ for(i in colnames(CrabSouth_allele)[2:length(CrabSouth_allele)])
               paste("BGC Files/South_pop",i,"_allct_bgc.txt",sep=""), row.names=F, col.names=F, quote=F, sep='\t')
 }
 
-#Mixed Populations * failes due to monomorphic allele issue
-# for(i in colnames(CrabMixed_allele)[2:length(CrabMixed_allele)])
-# {
-#   write.table(data.frame(Loci=LocusMixed,filler=rep('#',length(LocusMixed)),PopOdd=Mixed_Odd[,i],PopEven=Mixed_Even[,i]),
-#               paste("Mixed_pop",i,"_allct_bgc.txt",sep=""), row.names=F, col.names=F, quote=F, sep='\t')
-# }
+#Create the admixed file format --------------
+CrabMixed <- import2genind('Outliers_AdmixedIn.str', onerowperind=F, n.ind=43, n.loc=117, row.marknames=1, col.lab=1, col.pop=2, ask=F)
+CrabMixed <- genind2genpop(CrabMixed) ### I think you have a monomophic allele or something. Not an even number of alleles. 
+head(CrabMixed) # should be ab 1 x 230 matrix 
+
+CM_Data=t(CrabMixed@tab)
+CrabMixed_allele <- data.frame(Allele=rownames(CM_Data))
+CrabMixed_allele <- cbind(CrabMixed_allele,CM_Data)
+rownames(CrabMixed_allele) <- NULL ### ** issue here. There seems to be a missing allele for one of the loci
+
+LocusMixed1 <- as.character(CrabMixed_allele$Allele)
+LocusMixed1 <- lapply(strsplit(LocusMixed1, '.', fixed=TRUE), '[[', 1)
+
+#which have only one allele
+table(unlist(LocusMixed1))[which(as.vector(table(unlist(LocusMixed1)))==1)] # here we see the issue with locus 3657 5488 6798 7068  946 all only have one allele
+names(table(unlist(LocusMixed1))[which(as.vector(table(unlist(LocusMixed1)))==1)]) #names of the monomorphic alleles
+
+MonoAlleles <- names(table(unlist(LocusMixed1))[which(as.vector(table(unlist(LocusMixed1)))==1)])
+#LocusMixed2 <- LocusMixed[rep(c(TRUE,FALSE),nrow(CrabMixed_allele)/2)]
+LocusMixed2 <- unlist(LocusMixed1)
+
+LocusMixed3 <- LocusMixed2[!(LocusMixed2 %in% MonoAlleles)]
+table(unlist(LocusMixed3))[which(as.vector(table(unlist(LocusMixed3)))==1)] #check to see if it worked (Should be nothing)
+
+#remove repeats
+LocusMixed <- LocusMixed3[rep(c(TRUE,FALSE),(nrow(CrabMixed_allele)-length(MonoAlleles))/2)]
+
+# fixed up structure file where I added a header for the crab ID and the population
+MixedStruct1 <- read.table("Outliers_AdmixedIn.txt",header=T)
+Loci=gsub("X","",as.character(colnames(MixedStruct1)[3:length(MixedStruct1)]))
+colnames(MixedStruct1)[3:length(MixedStruct1)]=Loci
+
+MixedStruct=MixedStruct1[,c(colnames(MixedStruct1)[1:2],LocusMixed)]
+
+MixedStruct%>%group_by(Pop,ID)%>%summarise
 
 
+# function to assign a binary 0 1 or 2 scoring where 2 is the larger allele (2 or 4) and 1 is the smaller allele (1 or 3)
+aCount <- function(x){
+  if(length(unique(as.numeric(x[c("allele1","allele2")])))==1 &
+     sum(as.numeric(x[c("allele1","allele2")])!= -9)>1 &
+     unique(as.numeric(x["allele1"])==as.numeric(x["alleleMin"]))){return(c(1,1))} # same alleles (small)
+  
+  if(length(unique(as.numeric(x[c("allele1","allele2")])))==1 &
+     sum(as.numeric(x[c("allele1","allele2")])!= -9)>1 &
+     unique(as.numeric(x["allele1"])==as.numeric(x["alleleMax"]))){return(c(2,2))} # same alleles (small)
+  
+  if(as.numeric(x[,"allele1"])>as.numeric(x[,"allele2"])){return(c(2,0))}
+  if(as.numeric(x[,"allele1"])<as.numeric(x[,"allele2"])){return(c(0,2))}
+  if(unique(as.numeric(x[c("allele1","allele2")]))== -9){return(c(0,0))}
+}
+
+#This will create the file structure specified by the Mixed File structure data according to the BGC manual
+MixedData <- NULL
+for(col in names(MixedStruct)[3:length(MixedStruct)])
+  {
+  temp1 <- MixedStruct[,c("ID","Pop",col)]
+
+  Locushold <- paste("locus ",col,sep="") # Start the locus data
+    
+    for (i in unique(MixedStruct$Pop)) # each populatoin
+      {
+      temp2 <- filter(temp1,Pop==i) # subset for the population
+      
+      #Reformat the data for one row for each individaul (ID, Pop, Allele1, Allele2)
+      temp3 <- data.frame(ID=temp2[seq(1,nrow(temp2),2),"ID"],
+                          Pop=temp2[seq(1,nrow(temp2),2),"Pop"],
+                          allele1=temp2[seq(1,nrow(temp2),2),col],
+                          allele2=temp2[seq(2,nrow(temp2),2),col],
+                          alleleMax=max(temp2[,col],na.rm=T),
+                          alleleMin=min(temp2[which(temp2[,col]>(-1)),col],na.rm=T))
+      
+      temp4 <- as.data.frame(temp3%>%group_by(ID)%>%do(col1=aCount(.)[1],col2=aCount(.)[2])%>%ungroup())
+      
+      temp5 <- paste(temp4[,"col1"],temp4[,"col2"],sep=" ")
+      
+      Locushold <- c(Locushold,paste("pop ",i,sep=""),temp5)
+    } #end of population loop
+  
+    MixedData <- c(MixedData,Locushold) # add each successive locus 
+  } #end of locus loop
+  
+        
+write.table(MixedData,"MixedData_Crab.txt",sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
