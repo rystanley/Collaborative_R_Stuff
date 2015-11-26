ab10a47b62f2372997148ca6b4f4674a17751438
NickJeff13 <nick.w.jeffery3@gmail.com>
2015-11-26 12:57:09 -0400

    Plotting script to make a NH membership plot

---
+++ NewHybrids_Plotting Script.R
@@ -0,0 +1,33 @@
+  ## This file will create a structure like plot for "New Hybrids" ouput
+  
+  ##load libraries -----------
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(grid)
+
+## load data -----------
+nhdata <- read.csv("Outlier_NewHybsresults.csv")
+
+#Change column names
+names(nhdata)[2] <- "Sweeps"
+
+#Melt the data for plotting
+nDat <- melt(nhdata[,],id=c("Pop","Sweeps"))
+nDat$Pop=factor(nDat$Pop,levels=c("TKT","NWH","CBI","KJI","CLH","MBO","BRN","SYH","BDB","PLB","SGB"))
+
+#Plot the data
+p1=ggplot(nDat,aes(x=factor(Sweeps),y=value,fill=variable,group=Pop)) + 
+  geom_bar(position = "stack",stat = "identity",width=1)+
+  theme_bw()+scale_y_continuous(expand = c(0,0))+
+  facet_grid(~Pop,scales="free")+
+  theme(axis.text.x = element_blank(),
+        axis.ticks=element_blank(),
+        panel.margin = unit(0.1, "lines"),
+        legend.position="bottom")+
+  labs(x="Population",y="",fill="");p1
+
+#Save plot  
+ggsave("c:/Users/Nick/Desktop/NewHybrids_Output.png",
+       width=8.5,height=7.5,p1)
+
