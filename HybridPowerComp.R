75d7fa5a6381fac3acd03b570acfd0b683107817
rystanley <rstanley@mun.ca>
2016-02-11 15:13:39 -0400

    function

--- HybridPowerComp.R
+++ HybridPowerComp.R
@@ -1,4 +1,4 @@
-Hybridpower <-function(dir,filetag="",Threshold=NULL) {
+Hybridpower_comparison <-function(dir,filetag="",Thresholds=c(0.5,0.6,0.7,0.8,0.9),addThresh=FALSE){
   
 ## this function will estimate the power of assignment success and associated variability from New Hybrids for 6 possible classes
 #  Pure1, Pure2, F1, F2, BC1, BC2. The code is based on different simulations of individuals and repeat runs through New Hybrids (NH)
@@ -9,11 +9,11 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
 
 ## filetag - this is a name tag which will be added to the plots
 
-## threshhold - this is a theshold which will be added to the plots showing the assignment success for different levels of probability of a given 
-##              class estimated by NH. Default is (NULL) so if nothing specified it will not add this to the output plots (success ~ threshold by class)
+## Thresholds - this is a vector of thesholds (default c(0.6,0.7,0.8,0.9)) which will to compare among different thesholds to compare among different numbers of SNPs
+
+## addThresh - a logical vector (default: FALSE) which specifies whether the threshold values should be added to the summary plot.               
 
 
-  
   #Check to make sure the packages required are there and if not install them
   packages <- c("dplyr", "tidyr", "stringr","ggplot2","reshape2","grid","scales")
   if (length(setdiff(packages, rownames(installed.packages()))) > 0) { 
@@ -45,16 +45,24 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
               tempfiles <- list.files(paste0(filedir,i))
               pzfile <- tempfiles[grep("PofZ",tempfiles)]
               tempfile <- read.table(paste0(filedir,i,"/",pzfile),head=T)
+              
+              LociandAlleles <- tempfiles[grep("LociAndAlleles", tempfiles)]
+              LandAfile <- readChar(paste0(filedir, i, "/", LociandAlleles), file.info(paste0(filedir, i, "/", LociandAlleles))$size)
+              numLociExt <- str_extract(string = LandAfile, pattern = paste0("from ", "[:digit:]{1,5}", " loci")) 
+              numLociWorking <- gsub(x = numLociExt, pattern = "from ", replacement = "")
+              numLociWorking <- as.numeric(gsub(x = numLociWorking, pattern = " loci", replacement = ""))
+              
               #identify the simulation and repeat info
                 S_ident <- gsub("_","",str_extract(pzfile,paste0("_S","[:digit:]{1}","R","[:digit:]{1}","_")))
                 tempfile$sim <- substring(S_ident,1,2)
                 tempfile$rep <- substring(S_ident,3,4)
+                tempfile$nLoci <- numLociWorking
                 
                 tempfile <- tempfile[,-grep("IndivName",colnames(tempfile))] #delete IndivName
                 
               #rename the columns
-                colnames(tempfile) <- c("Indv","Pure1","Pure2","F1","F2","BC1","BC2","sim","rep")
-                tempfile=tempfile[,c("Indv","sim","rep","Pure1","Pure2","F1","F2","BC1","BC2")]# reorder
+                colnames(tempfile) <- c("Indv","Pure1","Pure2","F1","F2","BC1","BC2","sim","rep","nLoci")
+                tempfile=tempfile[,c("Indv","sim","rep","nLoci","Pure1","Pure2","F1","F2","BC1","BC2")]# reorder
                 
               #common order
                 if(sum(tempfile[1:200,"Pure1"],na.rm=T)<sum(tempfile[1:200,"Pure2"],na.rm=T)){
@@ -74,7 +82,7 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
 
 
     ## average and SD the  replicate runs of each simulation in New Hybrids
-      sim_data <- as.data.frame(output%>%group_by(sim,Indv)%>%summarise(Pure1_sd=sd(Pure1),Pure1=mean(Pure1),
+      sim_data <- as.data.frame(output%>%group_by(nLoci,sim,Indv)%>%summarise(Pure1_sd=sd(Pure1),Pure1=mean(Pure1),
                                                                        Pure2_sd=sd(Pure2),Pure2=mean(Pure2),
                                                                        F1_sd=sd(F1),F1=mean(F1),
                                                                        F2_sd=sd(F2),F2=mean(F2),
@@ -85,82 +93,59 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
       sim_means <- sim_data[,-grep("_sd",colnames(sim_data))]
     
     ## assign the classes to the data 
-      nIndv <- nrow(sim_means)/6/length(unique(sim_means$sim)) #number of simulated individuals (assumes the same number for each class)
-      sim_means$class=rep(rep(c("Pure1","Pure2","F1","F2","BC1","BC2"),each=nIndv),n=3)
+      nIndv <- nrow(sim_means)/6/length(unique(sim_means$sim))/length(unique(sim_means$nLoci)) #number of simulated individuals (assumes the same number for each class)
+      sim_means$class=rep(rep(c("Pure1","Pure2","F1","F2","BC1","BC2"),each=nIndv),times=3*length(unique(sim_means$nLoci)))
 
 
 #Compare the simulations using boxplots
     boxdata <- NULL
-    for (i in unique(sim_means$class))
+    for (i in unique(sim_means$nLoci))
     {
-      temp <- filter(sim_means,class==i)
-      tout <- data.frame(sim=temp$sim,Indv=temp$Indv,class=i,value=temp[,i])
+      for (j in unique(sim_means$class))
+      {
+      temp <- filter(sim_means,class==j,nLoci==i)
+      tout <- data.frame(sim=temp$sim,Indv=temp$Indv,class=j,nLoci=i,value=temp[,j])
       boxdata <- rbind(boxdata,tout)
+      }
     }
+    
+    boxdata$nLoci=factor(boxdata$nLoci)
 
     # Create pot
-    p1=ggplot(boxdata,aes(x=sim,y=value))+geom_jitter(size=0.1)+geom_boxplot(alpha=0.8,outlier.size = 0)+theme_bw()+facet_wrap(~class,nrow=3,scales="free_y")+
-    labs(y="Class probability",x="Simulation")+theme(strip.background = element_rect(fill="white"))
-    
+    p1=ggplot(boxdata,aes(x=nLoci,y=value,fill=sim))+geom_boxplot(alpha=0.8,outlier.size = 0)+theme_bw()+facet_wrap(~class,nrow=3,scales="free_y")+
+      labs(y="Class probability",x="Number of SNPs")+scale_fill_manual(values=c("white","white","white"))+
+      theme(strip.background = element_rect(fill="white"),legend.position="none")
+   
     #save plot
-  if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_AssignmentSuccess~simulation.pdf"),p1,height = 8,width = 10)}else
-  {ggsave(paste0(dir,"Figures/AssignmentSuccess~simulation.pdf"),p1,height = 8,width = 10)}
-
-## create the New Hybrids plot
-  
-      NH_melt <- melt(data = sim_means[,-grep("class",colnames(sim_means))], id.vars = c("Indv","sim")) ## melt the data to allow the data to be stacked by indivudal
-      colnames(NH_melt) <- c("Indv","sim", "PopProb", "CumProb") ## rename so that its prettier
-      
-      NH_melt$sim <- gsub("S","Simulation ",NH_melt$sim) # fix the simulation labels
-      
-      ## Plot colours
-      col.vec <- c("red", "blue", "grey", "green", "black", "yellow", "brown")
-      
-      
-      ## make a nice pretty little plot - and save that bad boy
-      #f.name <- paste0(res.folder, res.name, ".consensus.pdf")
-      #pdf(file = f.name)
-      
-      p2 <- ggplot(NH_melt, aes(x = Indv, y=CumProb, fill=PopProb))+
-      geom_bar(stat="identity", position = "stack") + 
-        scale_fill_manual(values=col.vec)+
-        labs(y="Cumulative Probability",x="Individual",fill="")+
-        scale_y_continuous(limits = c(0, 1), expand=c(0, 0)) + 
-        scale_x_continuous(expand=c(0,0)) +
-        facet_grid(sim~.)+
-        theme(axis.text.x = element_text(colour = "black"), axis.title.x = element_blank(), 
-              axis.text.y = element_text(colour = "black"), axis.title.y = element_text(size = 15, colour = "black"), 
-              strip.background = element_rect(fill="white",colour = "black"),panel.margin=unit(1, "lines"))
-      
-      #save the figure
-      if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_NewHybridsPlot~simulation.pdf"),p2,height = 8,width = 10)} else 
-      {ggsave(paste0(dir,"Figures/NewHybridsPlot~simulation.pdf"),p2,height = 8,width = 10)}
-
+  if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_AssignmentSuccess~simulation-nSNPs.pdf"),p1,height = 8,width = 10)}else
+  {ggsave(paste0(dir,"Figures/AssignmentSuccess~simulation-nSNPs.pdf"),p1,height = 8,width = 10)}
 
 ## Look at assignment success as a function of threshold probability
-    num.sim <- length(which(sim_means$sim=="S1"))/6
+    num.sim <- length(which(sim_means$sim=="S1"))/6/length(unique(sim_means$nLoci))
     
     ProbOutput <- NULL
-    for(i in unique(sim_means$sim)){
-      tempsub <- filter(sim_means,sim==i)
-        for(q in 50:99/100){ # probability of 50 - 99%
-         
-          farm.p <- length(which(tempsub$Pure1[1:200] > q))/num.sim
-          wild.p <- length(which(tempsub$Pure2[201:400] > q))/num.sim
-          F1.p <- length(which(tempsub$F1[401:600] > q))/num.sim
-          F2.p <- length(which(tempsub$F2[601:800] > q))/num.sim
-          farmBC.p <- length(which(tempsub$BC1[801:1000] > q))/num.sim
-          wildBC.p <- length(which(tempsub$BC2[1001:1200] > q))/num.sim
-          tempout <- data.frame(sim=i,level=q,prob=c(farm.p, wild.p,F1.p,F2.p,farmBC.p,wildBC.p),
-                                class=c("Pure1","Pure2","F1","F2","BC1","BC2")) 
-          ProbOutput <- rbind(ProbOutput,tempout)
-          
-        }
-      
-    }
+    for (s in unique(sim_means$nLoci)){
+        lsub <- filter(sim_means,nLoci == s)
+        for(i in unique(sim_means$sim)){
+          tempsub <- filter(lsub,sim==i)
+            for(q in 50:99/100){ # probability of 50 - 99%
+             
+              farm.p <- length(which(tempsub$Pure1[1:200] > q))/num.sim
+              wild.p <- length(which(tempsub$Pure2[201:400] > q))/num.sim
+              F1.p <- length(which(tempsub$F1[401:600] > q))/num.sim
+              F2.p <- length(which(tempsub$F2[601:800] > q))/num.sim
+              farmBC.p <- length(which(tempsub$BC1[801:1000] > q))/num.sim
+              wildBC.p <- length(which(tempsub$BC2[1001:1200] > q))/num.sim
+              tempout <- data.frame(nLoci=s,sim=i,level=q,prob=c(farm.p, wild.p,F1.p,F2.p,farmBC.p,wildBC.p),
+                                    class=c("Pure1","Pure2","F1","F2","BC1","BC2")) 
+              ProbOutput <- rbind(ProbOutput,tempout)
+              
+            } #end q loop
+          } #end i loop
+        } # end s loop
 
 # get the mean and standard error for the estimates of assignment succes based on NH probabilty among simulations    
-      FinalData <- data.frame(ProbOutput%>%group_by(level,class)%>%summarise(mprob = mean(prob,na.rm=T), 
+      FinalData <- data.frame(ProbOutput%>%group_by(nLoci,level,class)%>%summarise(mprob = mean(prob,na.rm=T), 
                                                                             sdprob = sd(prob,na.rm=T))%>%ungroup())
       FinalData$class <- factor(FinalData$class, levels=c("Pure1","Pure2","F1","F2","BC1","BC2")) # NH class
       
@@ -173,43 +158,43 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
       FinalData$class <- factor(FinalData$class,levels=c("Pure1","Pure2","F1","F2","BC1","BC2"))
     
     #plot the class groupings
-      p3 <- ggplot(FinalData,aes(x=level,y=mprob,col=class))+geom_line(lwd=1.25)+theme_bw()+facet_grid(group~.,scales="free_y")+
-        theme(legend.position="bottom",strip.background = element_rect(fill="white",colour = "black"))+labs(x="Probability threshold",y="Assignment success",col="Classification");p3
+      p3 <- ggplot(FinalData,aes(x=level,y=mprob,col=class))+geom_line(lwd=1.25)+theme_bw()+facet_grid(group~nLoci,scales="free_y")+
+        theme(legend.position="bottom",strip.background = element_rect(fill="white",colour = "black"))+
+        scale_color_brewer(palette = "Dark2")+
+        labs(x="Probability threshold",y="Assignment success",col="Classification")
 
     #plot if no threshold specified
-      if(length(Threshold)==0){
-      p4 <- ggplot(data=FinalData)+geom_line(aes(x=level,y=mprob),lwd=1.25)+
-        geom_line(aes(x=level,y=mprob+sdprob),lty=2)+geom_line(aes(x=level,y=mprob-sdprob),lty=2)+
+      if(addThresh){
+      p4 <- ggplot(data=FinalData)+geom_line(aes(x=level,y=mprob,col=factor(nLoci)),lwd=1.25)+
+        geom_line(aes(x=level,y=mprob+sdprob,col=factor(nLoci)),lty=2)+
+        geom_line(aes(x=level,y=mprob-sdprob,col=factor(nLoci)),lty=2)+
         theme_bw()+facet_wrap(~class,nrow=3,scales="free_y")+theme(strip.background = element_rect(fill="white",colour = "black"))+
-        labs(x="Probability threshold",y="Assignment success ± sd");p4
+        scale_color_brewer(palette = "Dark2")+
+        labs(x="Probability threshold",y="Assignment success ± sd",col="# Loci")+geom_vline(xintercept = Thresholds, lty=2)
       }
       
-    #plot if threshold specified 
-       if(length(Threshold)!=0){
-        #calcualte the lower assignment success for each class
-        lowerlim <- data.frame(FinalData%>%group_by(class)%>%
-                                 summarise(lim=min(mprob))%>%ungroup())
-        
-        threshlimits <- filter(FinalData,level==Threshold)
-        threshlimits$lower <- 0.5
-        threshlimits$threshold <- Threshold
-        threshlimits$x <- (threshlimits$threshold-0.5)/2
-        threshlimits <- merge(threshlimits,lowerlim,by = "class")
-        threshlimits$lim <- threshlimits$lim-threshlimits$sdprob
-        
-        p4 <- ggplot(data=FinalData)+
-          geom_line(aes(x=level,y=mprob),lwd=1.25)+
-          geom_line(aes(x=level,y=mprob+sdprob),lty=2)+
-          geom_line(aes(x=level,y=mprob-sdprob),lty=2)+
-          theme_bw()+facet_wrap(~class,nrow=3,scales="free_y")+
-          geom_segment(data=threshlimits,aes(x=lower,xend=level,y=mprob,yend=mprob),col="black")+
-          geom_segment(data=threshlimits,aes(x=level,xend=level,y=lim,yend=mprob),col="black")+
-          scale_y_continuous(expand=c(0.03,0))+scale_x_continuous(expand=c(0.04,0))+
-          labs(x="Probability threshold",y="Assignment success ± sd")+
-          theme(strip.background = element_rect(fill="white",colour = "black"))+
-          geom_text(data=threshlimits,aes(x=0.55,y = mprob-(mprob-lim)*0.1,label=round(mprob,2)));p4
+      if(!addThresh){
+        p4 <- ggplot(data=FinalData)+geom_line(aes(x=level,y=mprob,col=factor(nLoci)),lwd=1.25)+
+          geom_line(aes(x=level,y=mprob+sdprob,col=factor(nLoci)),lty=2)+
+          geom_line(aes(x=level,y=mprob-sdprob,col=factor(nLoci)),lty=2)+
+          theme_bw()+facet_wrap(~class,nrow=3,scales="free_y")+theme(strip.background = element_rect(fill="white",colour = "black"))+
+          scale_color_brewer(palette = "Dark2")+
+          labs(x="Probability threshold",y="Assignment success ± sd",col="# Loci")
       }
-
+      
+      ## mean plot
+      
+      #facet labels
+      FinalData$threshold <- paste0(FinalData$level*100,"%")
+      
+        p5 <- ggplot(filter(FinalData,level %in% Thresholds),aes(x=factor(nLoci),y=mprob,col=class,group=class))+
+        geom_point(size=2.5)+geom_path(lwd=0.9)+
+        geom_errorbar(aes(ymin=mprob-sdprob,ymax=mprob+sdprob),width=0.1)+
+        facet_grid(~threshold)+theme_bw()+
+        labs(x="Number of loci",y="Assignment success ± sd",col="Classification",group="")+scale_color_brewer(palette = "Dark2")+
+        theme(legend.position="bottom",strip.background = element_rect(fill="white",colour = "black"))
+      
+      
 #Save plots
 
     if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_AssinmentSuccess~level-class.pdf"),p3,height = 10,width = 8)} else 
@@ -217,5 +202,8 @@ Hybridpower <-function(dir,filetag="",Threshold=NULL) {
     
     if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_AssignmentSuccess~level-error.pdf"),p4,height = 10,width = 8)} else 
     {ggsave(paste0(dir,"Figures/AssignmentSuccess~level-error.pdf"),p4,height = 10,width = 8)}
+        
+    if(filetag!=""){ggsave(paste0(dir,"Figures/",filetag,"_AssignmentSuccess~z-loci.pdf"),p5,height = 10,width = 8)} else 
+    {ggsave(paste0(dir,"Figures/AssignmentSuccess~~z-loci.pdf"),p5,height = 10,width = 8)}
 
 } #end function
